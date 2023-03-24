8a86e78222c070ab1b46b25456e9e81e50af61fe
rsmikh <rs@decard.io>
2023-03-24 16:15:18 +0200

    feature [1611] created form component

--- src/common/types/global/index.d.ts
+++ src/common/types/global/index.d.ts
@@ -1,6 +1,8 @@
-declare module "*.module.scss" 
+declare module "*.module.scss";
 
-declare module "*.svg" {
-  const content: any;
-  export default content;
+declare module '*.svg' {
+  import React = require('react');
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
 }
