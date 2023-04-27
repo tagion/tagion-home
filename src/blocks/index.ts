706c7f349aed757fd9d350b3a693f5dfb2529341
rsmikh <rs@decard.io>
2023-04-27 13:26:47 +0300

    feature [1847] developed 'Timeline block'

--- src/blocks/index.ts
+++ src/blocks/index.ts
@@ -2,6 +2,7 @@ export * from "./explore-our-ecosystem";
 export * from "./financial-infrastrucure-next-gen";
 export * from "./secure-scalable-decentralised";
 export * from "./subscribe-to-our-newsletter";
+export * from "./timeline-block";
 export * from "./value-partners";
 export * from "./you-can-build-a-wallet";
 export * from "./you-can-participate";
