1964f7468dc77712ac628de807cbc0ffc770d2d6
rsmikh <rs@decard.io>
2023-05-19 15:12:48 +0300

    feature [1843] We are money block

--- src/common/types/global/index.d.ts
+++ src/common/types/global/index.d.ts
@@ -1,5 +1,7 @@
 declare module "*.module.scss";
 
+declare module "*.gif";
+
 declare module "*.svg" {
   import React = require("react");
   export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import classNames from "classnames/bind";
 
 import { Layout } from "../templates/layout";
-import { GradientSpotsWrapper } from "../components";
+import { GradientSpotsWrapper, IntroductoryBlock } from "../components";
 import {
   ExploreOurEcosystemBlock,
   FinancialInfrastrucureNextGenBlock,
@@ -19,6 +19,9 @@ import {
   newsletterSubscribeGradientSpotsProps,
 } from "../content";
 
+import { ReactComponent as HeroIcon } from "../assets/images/hero_icon.svg";
+import pointAnimation from "../assets/videos/point-animation.gif";
+
 import * as styles from "../styles/pages/index.module.scss";
 
 const cx = classNames.bind(styles);
@@ -26,6 +29,13 @@ const cx = classNames.bind(styles);
 const IndexPage = () => {
   return (
     <Layout>
+      <IntroductoryBlock
+        Icon={HeroIcon}
+        gifAnimation={pointAnimation}
+        title="We Are Pioneering Money"
+        description="Tagion empowers everyone everywhere to participate in a sustainable
+            economic world. Evolving trust with your participation."
+      />
       <FinancialInfrastrucureNextGenBlock />
       <YouCanBuildAWalletBlock />
       <GradientSpotsWrapper spots={gradientSpotsProps}>
