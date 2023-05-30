4b51fd0f0f17891d885c38b856bb49bb7bbd2482
Aleksandr Bohdanov <aleksandr.bohdanov@gmail.com>
2023-05-30 18:51:57 +0300

    Update api url

--- src/api/send-lets-talk-form.ts
+++ src/api/send-lets-talk-form.ts
@@ -5,11 +5,8 @@ import {
   ISendLetsTalkFormResponse,
 } from "../common/interfaces/lets-talk-form-request";
 
-const MAIL_SERVICE_ENDPOINT =
-  "https://dev-api.decard.io/mail-service/tagion-inquiry/";
-
 export const sendLetsTalkForm = (
   payload: ISendLetsTalkFormPayload
 ): Promise<AxiosResponse<ISendLetsTalkFormResponse>> => {
-  return axios.post(MAIL_SERVICE_ENDPOINT, payload);
+  return axios.post(process.env.GATSBY_MAIL_SERVICE_ENDPOINT as string, payload);
 };
