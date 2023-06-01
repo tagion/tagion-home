d3ebdff2c558a6b69bb5aad95f6e623ec9152353
rsmikh <rs@decard.io>
2023-06-01 17:20:02 +0300

    updated logo according to design

--- src/components/introductory-block/introductory-block.tsx
+++ src/components/introductory-block/introductory-block.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import classNames from "classnames/bind";
 
-import mainPageIntroSvg from "../../assets/images/main-page-intro.svg";
+import mainPageIntro from "../../assets/images/main-page-intro.png";
 // import { ReactComponent as HeroSvg } from "../../assets/images/hero_icon.svg";
 
 import * as styles from "./introductory-block.module.scss";
@@ -31,7 +31,7 @@ export const IntroductoryBlock: React.FC<InputProps> = ({
       )}
       <div className={cx("content")}>
         <div className={cx("icon_wrapper")}>
-          <img src={mainPageIntroSvg} className={cx("icon")} />
+          <img src={mainPageIntro} className={cx("icon")} />
         </div>
         <div className={cx("text_wrapper")}>
           <div className={`${cx("title")} title-font`}>{title}</div>
