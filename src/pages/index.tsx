0ef09e318763ab6ada0097d0a62bfa8ff1a9c36f
rsmikh <rs@decard.io>
2023-06-13 15:35:33 +0300

    [2128] developed About + logo block

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,6 +14,7 @@ import {
 import { mainPageGradients } from "../content";
 
 import pointAnimation from "../assets/videos/point-animation.gif";
+import mainPageIntro from "../assets/images/main-page-intro.png";
 
 import * as styles from "../styles/pages/index.module.scss";
 import "react-toastify/dist/ReactToastify.css";
@@ -29,9 +30,16 @@ const IndexPage = () => {
       >
         <IntroductoryBlock
           gifAnimation={pointAnimation}
+          img={mainPageIntro}
           title="We Are Pioneering Money"
           description="Tagion empowers everyone everywhere to participate in a sustainable
             economic world. Evolving trust with your participation."
+          classNames={{
+            imgWrapper: cx("main_img_wrapper"),
+            textWrapper: cx("main_text_wrapper"),
+            content: cx("main_content"),
+            introductoryBlock: cx("main_introductory_block"),
+          }}
         />
       </GradientSpotsWrapper>
       <FinancialInfrastrucureNextGenBlock />
