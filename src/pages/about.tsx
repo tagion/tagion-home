4bf124bb6fb89969a251b54ffe22bc1da81139fb
rsmikh <rs@decard.io>
2023-06-19 12:41:02 +0300

    added coming soon component on About page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,50 +1,12 @@
 import React from "react";
-import classNames from "classnames/bind";
 
 import { Layout } from "../templates/layout";
-import { GradientSpotsWrapper, IntroductoryBlock } from "../components";
-import { mainPageGradients } from "../content";
-
-import aboutPageIntro from "../assets/images/about-page/about-page-intro.png";
-import {
-  LetsTalkBlock,
-  SubscribeToOurNewsletterBlock,
-  ValuePartnersBlock,
-  YouCanParticipateBlock,
-} from "../blocks";
-
-import * as styles from "../styles/pages/about.module.scss";
-
-const cx = classNames.bind(styles);
+import { ComingSoon } from "../components";
 
 const AboutPage = () => {
   return (
-    <Layout withPaddingTop={false}>
-      <GradientSpotsWrapper
-        gradients={mainPageGradients.introductoryBlock}
-        disableMainSidePaddings
-      >
-        <IntroductoryBlock
-          title="About"
-          description="Five years ago, we were dreaming of a fairer, more efficient and inclusive monetary system and banking infrastructure. Today, we’re building our dream - a decentralised, scalable and secure money-based network with real human interactions. And the best part, is that we are building it together with you. "
-          img={aboutPageIntro}
-          classNames={{
-            imgWrapper: cx("about_img_wrapper"),
-            content: cx("about_content"),
-            textWrapper: cx("about_text_wrapper"),
-            introductoryBlock: cx("about_introductory_block"),
-          }}
-        />
-      </GradientSpotsWrapper>
-      <div className="title-font">Our vision and mission</div>
-      <ValuePartnersBlock />
-      <YouCanParticipateBlock />
-      <GradientSpotsWrapper
-        gradients={mainPageGradients.subscribeToNewsletterBlock}
-      >
-        <LetsTalkBlock />
-        <SubscribeToOurNewsletterBlock />
-      </GradientSpotsWrapper>
+    <Layout withPaddingTop={false} isHeaderShownOnTop>
+      <ComingSoon />
     </Layout>
   );
 };
