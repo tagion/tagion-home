fcfdedfcb0bf82a2cc1d8f7b4f028fd6ba924200
rsmikh <rs@decard.io>
2023-06-30 12:56:17 +0300

    [2209] fixed Left arrow button remains unclickable when user scrolls "Meet our contributors" block to the right

--- src/components/company-swiper/company-swiper.tsx
+++ src/components/company-swiper/company-swiper.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import classNames from "classnames/bind";
 
-import { CustomSwiper } from "../../components";
+import { CustomSwiper } from "..";
 
 import * as styles from "./company-swiper.module.scss";
 
