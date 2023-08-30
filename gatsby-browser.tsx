f6cddfea89a7bfefcf6bc2adb952fec2e0154cdd
Rostyslav Smikh <rs@decard.io>
2023-08-30 15:04:15 +0300

    [2418] Ecosystem page paddings

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,5 +1,10 @@
 import "./src/styles/index.scss";
 
+export const onInitialClientRender = () => {
+  console.log("test - onInitialClientRender was executed");
+  window.scrollTo(0, 0);
+};
+
 export const shouldUpdateScroll = () => {
   console.log("test - shouldUpdateScroll was executed");
   window.scrollTo(0, 0);
