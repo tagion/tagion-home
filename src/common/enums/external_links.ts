7613dbce1e9121871fee3dabedafd8aec64f78c5
Rostyslav Smikh <rs@decard.io>
2024-09-23 13:25:20 +0300

    fixed issue with article

--- src/common/enums/external_links.ts
+++ src/common/enums/external_links.ts
@@ -9,7 +9,7 @@ export enum ExternalLinks {
   DISCORD = "https://discord.gg/za2hb62quR",
   TWITTER = "https://twitter.com/TagionOfficial",
   TELEGRAM = "https://t.me/tagionChat",
-  MEDIUM = "https://tagion.medium.com/",
+  MEDIUM = "https://tagion.medium.com",
 
   START_CONTRIBUTING_JOIN_BUTTON = "https://discord.gg/QxbkYjtvER",
   ELINOR_OSTROMS_PRINCIPLES = "https://www.researchgate.net/publication/265017003_Elinor_Ostrom%27s_Legacy_Governing_the_Commons_and_the_Rational_Choice_Controversy",
